bce8a7d20208b553e9fc642b9984c426e67b8097
fiveroom <lkzipsea@163.com>
2019-12-12 01:17:53 +0800

    input

--- house-rent/src/views/Home.vue
+++ house-rent/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    nihao 
+    <Login />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Login from './Login.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+      Login,
   }
 }
 </script>
---
+++ house-rent/src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="login-from">
+        <MyInput id="name" placeholder="账号手机号" />
+        <MyInput id="pwd" placeholder="密码" type="password" />
+        <div>
+             <button>登录</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import MyInput from '../components/MyInput';
+export default {
+    components: {
+        MyInput,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.login-from{
+    width: 330px;
+    padding: 20px;
+    border: 1px solid red;
+    margin: 20px auto;
+}
+</style>
